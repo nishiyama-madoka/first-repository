8746935f6aecdc1207439361bbd6a2c18b1965aa
nishiyama-madoka <168533199+nishiyama-madoka@users.noreply.github.com>
2024-05-10 15:29:51 +0900

    Create ProfileMapper.java

---
+++ ProfileMapper.java
@@ -0,0 +1,18 @@
+package com.example.profileapp;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.profileapp.UserAddRequest;
+import com.example.profileapp.Person;
+
+@Mapper
+public interface ProfileMapper {
+
+	/**
+     * ユーザー情報登録
+     * @param userRequest 登録用リクエストデータ
+     */
+    void save(UserAddRequest userRequest);
+	
+}
