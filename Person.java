df9610f40631b2a159cb380ad1a16f6cd10c6cf1
nishiyama-madoka <168533199+nishiyama-madoka@users.noreply.github.com>
2024-05-10 09:56:54 +0900

    Update Person.java
    create entity

--- Person.java
+++ Person.java
@@ -1,1 +1,25 @@
+package com.example.profileapp;
+//entity
+import java.io.Serializable;
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class Person implements Serializable {
+
+	private Long id;
+	
+	private String email;
+	
+	private String name;
+	
+	private String selfIntroduction;
+	
+	private String password;
+	
+	private Date createdAt;
+	
+	private Data updatedAt;
+	
+}
