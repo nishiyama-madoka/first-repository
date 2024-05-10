590b870259ca0427e6120563d16a96ae251e40c4
nishiyama-madoka <168533199+nishiyama-madoka@users.noreply.github.com>
2024-05-10 15:40:23 +0900

    Update move top page AppController.java

--- AppController.java
+++ AppController.java
@@ -5,28 +5,42 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class AppController {
-	//新規登録画面
-	@RequestMapping("/")
+	@Autowired
+	private ProfileService profileService;
+//新規登録画面
+	@GetMapping(value = "/signin")
+	public String displayAdd(Model model) {
+        model.addAttribute("userAddRequest", new UserAddRequest());
+        return "/signin";
+    }
+	
+	 @GetMapping(value = "/top")
+	    public String displaytop() {
+	     
+	        return "/top";
+	    }
+	
+	@RequestMapping("/signin")
 	public String signin() {
 		return "signin";
 	}
 	
 	//エラー文
-	 @RequestMapping(value = "/", method = RequestMethod.POST)
+	 @RequestMapping(value = "/signin", method = RequestMethod.POST)
 	    public String create(@Validated @ModelAttribute UserAddRequest userRequest, BindingResult result, Model model) {
 	        if (result.hasErrors()) {
 	             //入力チェックエラーの場合
@@ -39,10 +53,8 @@ public class AppController {
 	        }
 	        }
 	
-	//トップ画面
-	@RequestMapping("/top")
-	public String topLoggedIn() {
-		return "topLoggedIn";
-	}
+	// ユーザー情報が登録できた場合
+     profileService.save(userRequest);
+     return "redirect:/top"; //トップ画面へ遷移するように変更
 	
 }
