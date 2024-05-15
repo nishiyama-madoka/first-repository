61a7beb1370ae3f7bb8cda741446a8dafccb8f3a
nishiyama-madoka <168533199+nishiyama-madoka@users.noreply.github.com>
2024-05-15 17:05:57 +0900

    ハッシュ化 ProfileService.java

--- ProfileService.java
+++ ProfileService.java
@@ -2,6 +2,7 @@ package com.example.profileapp;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +14,17 @@ public class ProfileService {
     @Autowired
     private ProfileMapper userInfoMapper;
 
-  
+  @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     /**
      * ユーザ情報登録
      * @param userAddRequest リクエストデータ
      */
     public void save(UserAddRequest userAddRequest) {
+	    String encodedPassword = passwordEncoder.encode(userAddRequest.getPassword());
+        userAddRequest.setPassword(encodedPassword);
         userInfoMapper.save(userAddRequest);
     }
 
