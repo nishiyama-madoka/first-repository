4ff18f7fea75ebc4b1fbb06b3e5689e1994849bb
nishiyama-madoka <168533199+nishiyama-madoka@users.noreply.github.com>
2024-05-15 17:04:56 +0900

    ハッシュ化 Person.java

--- Person.java
+++ Person.java
@@ -2,12 +2,17 @@ package com.example.profileapp;
 //entity
 import java.io.Serializable;
 import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.Data;
-
+@Entity
 @Data
+@Table(name="users")	
 public class Person implements Serializable {
 
+	@Id
 	private Long id;
 	
 	private String email;
